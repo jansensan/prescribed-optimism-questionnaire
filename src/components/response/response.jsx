823f241484b46fc45e7c9107520378e8bfa7123d
Mat Janson Blanchet <mat.jb@jansensan.net>
2018-11-06 17:04:45 +0100

    Reduced complexity to set min/max values

--- src/components/response/response.jsx
+++ src/components/response/response.jsx
@@ -17,18 +17,15 @@ export default class Response extends Component {
       <div className="response">
         <p>{this.getLabel()}</p>
         <form action="">
-          {
-            this.isLikertScale() &&
-              <input
-                type="range"
-                name={this.getInputName()}
-                min={this.getMin()}
-                max={this.getMax()}
-                step={this.getStepSize()}
-                value={this.state.value}
-                onChange={this.onRangeChanged.bind(this)}
-              />
-          }
+          <input
+            type="range"
+            name={this.getInputName()}
+            min={this.getMin()}
+            max={this.getMax()}
+            step={this.getStepSize()}
+            value={this.state.value}
+            onChange={this.onRangeChanged.bind(this)}
+          />
         </form>
       </div>
     );
@@ -36,6 +33,8 @@ export default class Response extends Component {
 
   componentDidMount() { 
     this.isComponentMounted = true;
+    this.state.value = this.getInitialValue();
+    this.update();
   }
 
 
@@ -46,24 +45,7 @@ export default class Response extends Component {
   }
 
   getInitialValue() {
-    let unitType = _.get(this.props, 'type.unit');
-    let numSteps = _.get(this.props, 'type.numSteps');
-
-    let val = 0;
-    switch (unitType) {
-      case 'uint':
-        val = Math.floor(numSteps / 2);
-        break;
-
-      case 'int':
-        val = 0;
-        break;
-
-      case 'percentage':
-        val = 50;
-        break;
-    }
-    return val;
+    return _.get(this.props, 'type.initVal');
   }
 
   getLabel() {
@@ -71,63 +53,15 @@ export default class Response extends Component {
   }
 
   getMin() {
-    let unitType = _.get(this.props, 'type.unit');
-
-    let min = 0;
-    switch (unitType) {
-      case 'uint':
-      case 'percentage':
-        min = 0;
-      break;
-
-      case 'int':
-        let numSteps = _.get(this.props, 'type.numSteps');
-        min = -Math.floor(numSteps / 2);
-        break;
-    }
-    return min;
+    return _.get(this.props, 'type.min');
   }
 
   getMax() {
-    let unitType = _.get(this.props, 'type.unit');
-    let numSteps = _.get(this.props, 'type.numSteps');
-
-    let max = 0;
-    switch (unitType) {
-      case 'uint':
-        max = numSteps;
-        break;
-
-      case 'int':
-        max = Math.floor(numSteps / 2);
-        break;
-
-      case 'percentage':
-        max = 100;
-        break;
-    }
-    return max;
+    return _.get(this.props, 'type.max');
   }
 
   getStepSize() {
-    let unitType = _.get(this.props, 'type.unit');
-
-    var stepSize = 1;
-    switch (unitType) {
-      case 'uint':
-      case 'int':
-        stepSize = 1;
-        break;
-
-      case 'percentage':
-        stepSize = 10;
-        break;
-    }
-    return stepSize;
-  }
-
-  isLikertScale() {
-    return _.get(this.props.type, 'scale') === 'likert';
+    return _.get(this.props, 'type.step');
   }
 
   setInitialValue() {
