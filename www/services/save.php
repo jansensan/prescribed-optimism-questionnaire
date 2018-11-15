b080ededc7c20a7a0d89724814aa2bf104558a61
Mat Janson Blanchet <mat.jb@jansensan.net>
2018-11-15 13:04:20 +0100

    Fixed save service issue (#2)

--- www/services/save.php
+++ www/services/save.php
@@ -1,12 +1,13 @@
 <?php
-  $dbHost = getSafeEnvVar('DB_HOST');
-  $dbName = getSafeEnvVar('DB_NAME');
-  $dbUsername = getSafeEnvVar('DB_USER');
-  $dbPassword = getSafeEnvVar('DB_PASSWORD');
-  $responsesTable = getSafeEnvVar('RESPONSES_TABLE');
+  $db_host = getSafeEnvVar('DB_HOST');
+  $db_name = getSafeEnvVar('DB_NAME');
+  $db_username = getSafeEnvVar('DB_USER');
+  $db_password = getSafeEnvVar('DB_PASSWORD');
+  $responses_table = getSafeEnvVar('RESPONSES_TABLE');
+
 
   // create database connection
-  $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+  $db = new mysqli($db_host, $db_username, $db_password, $db_name);
 
   // check connection
   if ($db->connect_error) {
@@ -16,11 +17,15 @@
   } else {
     // query to save to db
     $data = $_POST["data"];
-    $query_string = "INSERT INTO " . $responsesTable
-      . " (data) " . " VALUES (" . $data . ")";
-
+    $query_string = "INSERT INTO `" . $responses_table . "`" . " (`data`) " . " VALUES ('" . $data . "')";
     $result = $db->query($query_string);
 
     echo $result;
+  }
+
+
+  // methods definitions
+  function getSafeEnvVar($var_name) {
+    return getenv($var_name) ? getenv($var_name) : 'undefined';
   }
 ?>
