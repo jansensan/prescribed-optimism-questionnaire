f795d9f50b4341bd9bb3d07f9801c88452df7f95
Mat Janson Blanchet <mat.jb@jansensan.net>
2018-11-21 18:05:45 +0100

    Added intro text

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -22,13 +22,17 @@ export default class Intro extends Component {
   render() {
     return (
       <div className={this.getComponentCSSClasses()}>
-        <h1 tabIndex="-1">Intro</h1>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        <div className="buttons-wrapper">
-          <button
-            className="btn-primary next-btn"
-            onClick={this.onBeginRequested.bind(this)}
-          >Begin</button>
+        <div lang="en">
+          <h1 tabIndex="-1">Intro</h1>
+          <p>You will be presented with eight different hypothetical situations. Carefully read each situation and answer the questions. This survey will only take a few minutes to complete.</p>
+          <p>We will not collect any nominal data, and please be assured that the information you provide will be kept in the strictest confidentiality, and will not be used for commercial or any other purposes beyond this academic study.</p>
+          <p>Thank you for participating in our important survey!</p>
+          <div className="buttons-wrapper">
+            <button
+              className="btn-primary next-btn"
+              onClick={this.onBeginRequested.bind(this)}
+            >Begin</button>
+          </div>
         </div>
       </div>
     )
